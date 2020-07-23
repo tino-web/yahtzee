ce4b4e79bd828b4997d21347b75925ae9bc7a46f
Tino <rijnders.martijn@gmail.com>
2020-07-23 22:06:26 +0700

    Add highscores list

--- src/components/LeaderBoard/LeaderBoard.jsx
+++ src/components/LeaderBoard/LeaderBoard.jsx
@@ -4,7 +4,7 @@ import useHighScores from '../../hooks/useHighScores';
 function LeaderBoard() {
   const [fieldScore, setFieldScore] = useState('');
   const [name, setName] = useState('');
-  const { submitHighScores, clearHighScores } = useHighScores();
+  const { highScores, submitHighScores, clearHighScores } = useHighScores();
 
   function handleScoreChange(event) {
     setFieldScore(event.target.value);
@@ -14,9 +14,27 @@ function LeaderBoard() {
     setName(event.target.value);
   }
 
+  const highScoresList = highScores.map((item) => (
+    <tr key={item.scoreId}>
+      <td>{item.name}</td>
+      <td>{item.score}</td>
+    </tr>
+  ));
+
   return (
     <>
       <h1>Leaderboard</h1>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {highScoresList}
+        </tbody>
+      </table>
       <input value={fieldScore} type='number' onChange={handleScoreChange} />
       <input value={name} type='text' onChange={handleNameChange} />
       <button type='button' onClick={() => submitHighScores(fieldScore, name)}>Submit</button>
